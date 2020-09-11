9697fe1f9c727c41c4e2e892584f5262711d9a77
Eduardo Flores <edfloreshz@gmail.com>
2020-09-11 08:56:10 -0700

    Added linker and asm files, using cargo-xbuild to build

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,24 @@
 #![no_std]
 #![no_main]
+#![feature(global_asm)]
 
-use core::panic::PanicInfo;
-extern crate rlibc;
+use core::ptr;
+mod panic;
 
-static HELLO: &[u8] = b"Hello World!";
+const UART0: *mut u8 = 0x0900_0000 as *mut u8;
+global_asm!(include_str!("start.s"));
 
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
-  let vga_buffer = 0xb8000 as *mut u8;
-  for (i, &byte) in HELLO.iter().enumerate() {
-    unsafe {
-      *vga_buffer.offset(i as isize * 2) = byte;
-      *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-    }
-  }
+pub extern "C" fn not_main() {
+  let out_str = b"HyperOS v0.0.1";
+  write(out_str);
   loop {}
 }
 
-/// This function is called on panic.
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-  loop {}
+fn write(out_str: &[u8]) {
+  for byte in out_str {
+    unsafe {
+      ptr::write_volatile(UART0, *byte);
+    }
+  }
 }
